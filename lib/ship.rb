66a89f1a623ea895a7beaed11565808e7550b2f2
Jeffrey Gu <jeffwgu@gmail.com>
2015-01-06 17:32:33 -0700

    inital commit for this branch

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,19 +1,24 @@
 require "byebug"
 class Ship
-	attr_accessor :coordinates, :hits, :size, :sunk
+	attr_accessor :coordinates, :hits, :size, :sunk, :board_size
 
-	def initialize(other_ship = nil)
-		@other_ship = other_ship
+	def initialize(other_ship_array = nil, board_size = 4)
+		@other_ship_array = other_ship_array 
 		@coordinates = ["A1", "A2"]
 		@size = @coordinates.length
 		@sunk = 0
 		@hits = 0
-		@sample_array = ["A", "B", "C"]
+    @board_size = board_size
+    @sample_array = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"][0..(@board_size - 1)]
 	end
 
 	def random_1x2
-		@coordinates[0] = @sample_array.sample + rand(1..3).to_s
-		if rand > 0.5
+    @coordinates[0] = @sample_array.sample + rand(1..@board_size).to_s
+    while @coordinates[0] == ((@sample_array[-1] + (@board_size - 1).to_s) || (@sample_array[-2] + (@board_size).to_s) || (@sample_array[-1] + @board_size.to_s))
+    	@coordinates[0] = @sample_array.sample + rand(1..@board_size).to_s
+    end
+
+    if self.on_bottom_edge?(@coordinates[0],2)
 			@coordinates [1] = @coordinates[0][0] + @coordinates[0][1].next 
 		else
 			@coordinates [1] = @coordinates[0][0].next + @coordinates[0][1]
@@ -21,30 +26,92 @@ class Ship
 		@coordinates
 	end
 
-	def random_1x3
-		@coordinates[0] = @sample_array[0..1].sample + rand(1..2).to_s
-		while @other_ship.coordinates.include?(@coordinates[0])
+	# def random_1x3
+	# 	@coordinates[0] = @sample_array[0..1].sample + rand(1..2).to_s
+	# 	while @other_ship.coordinates.include?(@coordinates[0])
+	# 		@coordinates[0] = @sample_array[0..1].sample + rand(1..2).to_s
+	# 	end
+	# 	if horz_space_check(@coordinates[0],3)
+	# 		@coordinates[0][1] = (@coordinates[0][1].to_i - 3).to_s
+	# 		@coordinates[1] = @coordinates[0][0] + @coordinates[0][1].next
+	# 		@coordinates[2] = @coordinates[0][0] + @coordinates[1][1].next
+	# 	else
+	# 		@coordinates[0][1] = (@coordinates[0][1].to_i - 3).to_s
+	# 		@coordinates[1] = @coordinates[0][0].next + @coordinates[0][1]
+	# 		@coordinates[2] = @coordinates[1][0].next + @coordinates[0][1]
+	# 	end
+	# 	@coordinates
+	# end
+
+  def random_1xSize(size)
+    @coordinates[0] = @sample_array.sample + rand(1..@board_size).to_s
+		while @other_ship_array.flatten.include?(@coordinates[0]) && in_corner?(@coordinates[0], size)
 			@coordinates[0] = @sample_array[0..1].sample + rand(1..2).to_s
 		end
-		if horz_space_check(@coordinates[0],3)
-			@coordinates[0][1] = (@coordinates[0][1].to_i - 3).to_s
-			@coordinates[1] = @coordinates[0][0] + @coordinates[0][1].next
-			@coordinates[2] = @coordinates[0][0] + @coordinates[1][1].next
-		else
-			@coordinates[0][1] = (@coordinates[0][1].to_i - 3).to_s
-			@coordinates[1] = @coordinates[0][0].next + @coordinates[0][1]
-			@coordinates[2] = @coordinates[1][0].next + @coordinates[0][1]
-		end
-		@coordinates
+  end
+
+  def linear_placer(coordinate, vert_hor)
+    new_coordinate = nil
+    if vert_hor == "v"
+      new_coordinate = coordinate[0].next + coordinate[1]
+    else
+      new_coordinate = coordinate[0] + coordinate[1].next
+    end
+    new_coordinate
+  end
+
+  def in_corner?(coordinate, ship_size)
+    the_corner = []
+    the_corner << (@sample_array[ - (ship_size-1)] + (@board_size - (ship_size - 2)).to_s)
+    
+    0.upto(ship_size - 3) do |x|      
+     the_corner << linear_placer(the_corner[x], "v")
+    end
+
+    array_length = the_corner.length
+    if array_length == 2
+     0.upto(1) do |x|      
+     the_corner << linear_placer(the_corner[x], "h")
+    end
+    else
+     0.upto(array_length * (array_length - 2)) do |x|      
+      the_corner << linear_placer(the_corner[x], "h")
+     end
+    end
+    return the_corner.include?(coordinate)
+  end
+
+  def on_bottom_edge?(coordinate, ship_size)
+    @sample_array[-(ship_size - 1)..-1].include?(coordinate[0])
+  end
+
+  def on_right_edge?(coordinate, ship_size)
+    ((@board_size - ship_size + 2)..@board_size).to_a.include?(coordinate[1].to_i)
+  end
+  
+  def straight?(array)
+    array.sort!
+	  straight_count = 0
+    1.upto(array.length-1) do |x|
+      if array[x-1][0].next + array[x-1][1] == array[x]
+        straight_count += 1
+      elsif array[x-1][0] + array[x-1][1].next == array[x]
+        straight_count += 1
+      end
+    end
+    true if straight_count == array.length - 1
 	end
 
-	def straight?
-		
-	end
-
-	def blocked?
-		
-	end
+  def blocked?(array)
+    blocked = false
+    array.each do |coordinate|
+      if @other_ship_array.flatten.include?(coordinate)
+        blocked = true
+        break
+      end
+    end
+    blocked
+  end
 
 	def horz_space_check(starting_coordinate, ship_size)
 		space_okay = true
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -8,31 +8,69 @@ require './lib/map'
 
 class ShipTest < MiniTest::Test
 	def setup
-		@battleship = Battleship.new
 		@ship_1x2 = Ship.new
 		@ship_1x3 = Ship.new(@ship_1x2)
 	end
 
-	def test_it_generates_a_predictable_2x2_ship
+	def test_it_generates_a_predictable_1x2_ship
 		assert_equal ["A1", "A2"], @ship_1x2.coordinates
 	end
 
 	def test_it_generates_a_random_1x2_ship
-		random_1x2 = Ship.new
+		random_1x2 = Ship.new(nil, 10)
 		puts random_1x2.random_1x2
 	end
 
-	def test_it_generates_a_1x3_not_on_top_of_the_1x2
-		puts @ship_1x3.random_1x3
-	end
+	# def test_it_generates_a_1x3_not_on_top_of_the_1x2
+	# 	puts @ship_1x3.random_1x3
+	# end
 
-	def test_space_check_checks_spaces_of_existing_ships_given_a_size
-		refute @ship_1x3.horz_space_check("A1",3)
-		assert @ship_1x3.horz_space_check("B1",3)
-	end
+	# def test_space_check_checks_spaces_of_existing_ships_given_a_size
+	# 	refute @ship_1x3.horz_space_check("A1",3)
+	# 	assert @ship_1x3.horz_space_check("B1",3)
+	# end
 
-	def test_it_generates_random_ships
-		puts @ship_1x2.random_1x2
-		puts @ship_1x3.random_1x3
-	end
+	# def test_it_generates_random_ships
+	# 	puts @ship_1x2.random_1x2
+	# 	puts @ship_1x3.random_1x3
+	# end
+
+  def test_it_straights
+    assert @ship_1x2.straight?(["A1","A2"]) 
+    assert @ship_1x2.straight?(["A1","B1"]) 
+    refute @ship_1x2.straight?(["A1","B2"])
+  end
+
+  # def test_it_blocks
+  #   refute @ship_1x3.blocked?(["B2","B3","B4"])
+  #   assert @ship_1x3.blocked?(["A1","A2","A3"])
+  #   assert @ship_1x3.blocked?(["A2","B2","C2"])
+  # end
+
+  def test_it_places_linearly_or_vertically
+     assert_equal "B2", @ship_1x3.linear_placer("A2","v")
+     assert_equal "A3", @ship_1x3.linear_placer("A2","h")
+  end
+
+  def test_is_the_given_ship_cornered
+    assert @ship_1x3.in_corner?("C3", 3)
+    refute @ship_1x3.in_corner?("C2", 3)
+    another_ship = Ship.new(nil, 5)
+    assert another_ship.in_corner?("E3", 4)
+    assert another_ship.in_corner?("C3", 4)
+    refute another_ship.in_corner?("C2", 4)
+  end
+
+  def test_it_knows_bottom_and_top_edges
+    new_ship = Ship.new(nil, 4)
+    assert new_ship.on_bottom_edge?("C2", 3)
+    refute new_ship.on_bottom_edge?("B2", 3)
+    assert new_ship.on_right_edge?("C3",3)
+    refute new_ship.on_right_edge?("C2",3)
+    new_ship_2 = Ship.new(nil, 6)
+    assert new_ship_2.on_bottom_edge?("C2", 5)
+    refute new_ship_2.on_bottom_edge?("B2", 5)
+    assert new_ship_2.on_right_edge?("C3", 5)
+    refute new_ship_2.on_right_edge?("C2", 5)
+  end
 end
